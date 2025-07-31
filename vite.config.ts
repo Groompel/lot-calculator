71b4c93fd81e045cb6555bf251547f457c776cff
mukhammed <kzdoandos@gmail.com>
2025-07-31 15:10:00 +0500

    feat: add pages config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
-  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.svg'],
+export default defineConfig(({ mode }) => {
+  // Set base path for GitHub Pages
+  const base = mode === 'github-pages' ? '/trading-calculator/' : '/';
+
+  return {
+    base,
+    plugins: [react()],
+    css: {
+      postcss: './postcss.config.js',
+    },
+    assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.svg'],
+  };
 });
